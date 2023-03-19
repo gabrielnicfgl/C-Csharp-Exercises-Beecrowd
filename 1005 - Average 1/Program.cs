c9062e20e704ed83d267c4b3da744b5deb314309
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-19 17:41:04 -0300

    resolução 1005

---
+++ 1005 - Average 1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Globalization;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        Double peso1 = 3.5;
+        Double peso2 = 7.5;
+
+        Double a = Convert.ToDouble(Console.ReadLine());
+        Double b = Convert.ToDouble(Console.ReadLine());
+        Double media = (a * peso1 + b * peso2) / 11;
+
+        Console.WriteLine($"MEDIA = {media.ToString("F5", CultureInfo.InvariantCulture)}");
+
+        Console.Read();
+    }
+
+}
