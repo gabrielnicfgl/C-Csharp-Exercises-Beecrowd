58345333869b8421bdd275b2925b15877f91cd21
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-19 16:31:07 -0300

    Resolução 1003

---
+++ 1003 - Simple Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int SOMA = a + b;
+        Console.WriteLine($"SOMA = {SOMA}");
+    }
+
+}
