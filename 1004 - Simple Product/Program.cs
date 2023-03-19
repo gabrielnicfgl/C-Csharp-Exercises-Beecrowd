7b51425967dc4b589d73b9da2b0896a2df4da759
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-19 16:55:43 -0300

    Resolução 1004

---
+++ 1004 - Simple Product/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int prod = a * b;
+        Console.WriteLine($"PROD = {prod}");
+    }
+
+}
