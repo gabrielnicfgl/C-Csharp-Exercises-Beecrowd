ef9e9a91e4a82b1d65b8dfcd55616acd595d03de
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-19 16:14:22 -0300

    Resolução 1002

---
+++ 1002 - Area of a Circle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        /*Double n = 3.14159;
+        Double r = Convert.ToDouble(Console.ReadLine());
+        Double a = n * Math.Pow(r, 2);
+        Console.WriteLine($"A = {a.ToString("F4", CultureInfo.InvariantCulture)}");
+    */
+
+        double n = 3.14159;
+        double R = Convert.ToDouble(Console.ReadLine());
+        double A = n * Math.Pow(R, 2.00);
+        string s = A.ToString("F4");
+        System.Console.WriteLine("A=" + s);
+
+    }
+
+}
