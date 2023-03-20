2e13a278d847412af2a2f5df8d66abd4ae79f6ae
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-19 21:02:30 -0300

    Resolução 1007

---
+++ 1007 - Difference/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        int d = Convert.ToInt32(Console.ReadLine());
+
+        int diferenca = a * b - c * d;
+
+        System.Console.WriteLine($"DIFERENCA = {diferenca}");
+    }
+
+}
