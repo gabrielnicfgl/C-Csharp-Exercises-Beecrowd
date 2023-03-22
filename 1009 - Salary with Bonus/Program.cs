66220a2ff28f41d4f84b58241e860faf806a8f52
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-22 16:57:10 -0300

    Resolução 1009

---
+++ 1009 - Salary with Bonus/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string name = Convert.ToString(Console.ReadLine());
+        Double salaryFixed = Convert.ToDouble(Console.ReadLine());
+        Double sales = Convert.ToDouble(Console.ReadLine());
+        Double salary = salaryFixed + sales * 15 / 100;
+        Console.WriteLine($"TOTAL = R$ {salary:F2}");
+
+    }
+}
