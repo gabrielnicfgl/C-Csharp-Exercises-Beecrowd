67b8b3dfc9ce6011ecd872034ff6e5a0e8a8c14a
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-22 16:33:26 -0300

    Resolução 1008

---
+++ 1008 - Salary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        int hoursWokerd = Convert.ToInt32(Console.ReadLine());
+        Decimal hourlyRate = Convert.ToDecimal(Console.ReadLine());
+        Decimal salary = hourlyRate * hoursWokerd;
+
+        System.Console.WriteLine($"NUMBER = {number} ");
+        System.Console.WriteLine($"SALARY = U$ {salary.ToString("F2", CultureInfo.InvariantCulture)}");
+
+    }
+}
