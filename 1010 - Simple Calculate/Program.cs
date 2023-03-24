a8a22a8eef6374f1a0bc4928e42f3966170b46c8
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-24 15:52:09 -0300

    Resolução 1010

---
+++ 1010 - Simple Calculate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] prod1 = Console.ReadLine().Split(' ');
+
+        int code = Convert.ToInt32(prod1[0]);
+        int unit = Convert.ToInt32(prod1[1]);
+        double price = Convert.ToDouble(prod1[2]);
+
+        string[] prod2 = Console.ReadLine().Split(' ');
+
+        int code2 = Convert.ToInt32(prod2[0]);
+        int unit2 = Convert.ToInt32(prod2[1]);
+        double price2 = Convert.ToDouble(prod2[2]);
+
+        double total = (unit * price) + (unit2 * price2);
+        System.Console.WriteLine($"VALOR A PAGAR: R$ {total:F2}");
+
+
+
+
+    }
+
+}
