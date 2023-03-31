2bd1ac19460552dcbf25663d4d1dfbf0cfbecbc1
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-31 18:21:40 -0300

    Resolução 1012

--- 1012 - Area/Program.cs
+++ 1012 - Area/Program.cs
@@ -12,11 +12,11 @@ class Uri
         double c = Convert.ToDouble(valores[2]);
         double pi = 3.14159;
 
-        double triangle = a * c / 2;
-        double circle = pi * Math.Pow(c, 2);
-        double trapezoid = (a + b) / 2 * c;
-        double square = Math.Pow(b, 2);
-        double rectangle = a * b;
+        double triangle = a * c / 2; // Area do triangulo é o produto do lado A e C dividido por dois
+        double circle = pi * Math.Pow(c, 2); //A área do circulo é o produto de PI e C²
+        double trapezoid = (a + b) / 2 * c; // A área do trapézio é o soma das bases (A e B) dividida por dois e multiplicada pela altura(C)
+        double square = Math.Pow(b, 2);// A área do quadrado é a exponenciação de um dos seus lados iguais.
+        double rectangle = a * b;// Nesse exemplo. é o produto entre o lado A e B
 
         System.Console.WriteLine($"TRIANGULO: {triangle:F3}");
         System.Console.WriteLine($"CIRCULO: {circle:F3}");
