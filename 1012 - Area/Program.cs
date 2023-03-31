ceb08d06d46cf1033b008d3fd2f2973e85c4bcca
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-31 18:18:30 -0300

    Resolução 1012

---
+++ 1012 - Area/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Uri
+{
+    static void Main(string[] args)
+    {
+        string[] valores = Console.ReadLine().Split(' ');
+
+        double a = Convert.ToDouble(valores[0]);
+        double b = Convert.ToDouble(valores[1]);
+        double c = Convert.ToDouble(valores[2]);
+        double pi = 3.14159;
+
+        double triangle = a * c / 2;
+        double circle = pi * Math.Pow(c, 2);
+        double trapezoid = (a + b) / 2 * c;
+        double square = Math.Pow(b, 2);
+        double rectangle = a * b;
+
+        System.Console.WriteLine($"TRIANGULO: {triangle:F3}");
+        System.Console.WriteLine($"CIRCULO: {circle:F3}");
+        System.Console.WriteLine($"TRAPEZIO: {trapezoid:F3}");
+        System.Console.WriteLine($"QUADRADO: {square:F3}");
+        System.Console.WriteLine($"RETANGULO: {rectangle:F3}");
+
+    }
+}
