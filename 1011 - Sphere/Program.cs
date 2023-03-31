c0b5894e345f88d3769a084b5635750b7b03da18
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-03-31 15:58:56 -0300

    Resolução 1011

---
+++ 1011 - Sphere/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class uri
+{
+    static void Main(String[] args)
+    {
+
+        const Double PI = 3.14159;
+        double a = Convert.ToDouble(Console.ReadLine());
+        double vol = (4 / 3.0) * PI * Math.Pow(a, 3);
+
+        System.Console.WriteLine($"VOLUME {vol:F3}"); // IMPORTANTE COLOCAR O NUMERO DA DIVISÃO DIVIDIDO POR UM NUMERO DECIMAL, CASO CONTRARIO, O PROGRAMA DIVIDE E TRUCA EXIBINDO APENAS A PARTE INTEIRA, OCASIONANDO NO ERRO DA EXIBIÇÃO CORRETA DO VALOR.
+
+
+
+
+    }
+}
