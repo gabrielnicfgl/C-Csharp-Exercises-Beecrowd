568b61303188df59f7c84efb9cfaf60b011f0db3
gabrielnicfgl <gabrielnicfgl@gmail.com>
2023-04-01 17:28:54 -0300

    Resolução 1013

---
+++ 1013 - The Greatest/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] vet = Console.ReadLine().Split(' ');
+        int a = int.Parse(vet[0]);
+        int b = int.Parse(vet[1]);
+        int c = int.Parse(vet[2]);
+
+        int ab = (a + b + Math.Abs(a - b)) / 2;
+        int abc = (ab + c + Math.Abs(ab - c)) / 2;
+
+        Console.WriteLine($"{abc}   eh o maior");
+
+    }
+
+}
+
